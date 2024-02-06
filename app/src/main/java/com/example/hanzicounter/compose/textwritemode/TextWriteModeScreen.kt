ee802bcef525a587a017a3f81f7f97417adf7966
Rodrigo Konzen Kirch <rodrigokirch@gmail.com>
2024-02-06 00:37:51 -0300

    Change button appearance

--- app/src/main/java/com/example/hanzicounter/compose/textwritemode/TextWriteModeScreen.kt
+++ app/src/main/java/com/example/hanzicounter/compose/textwritemode/TextWriteModeScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -55,12 +56,12 @@ fun TextWriteModeScreen(
                     ) {
                         Text(text = stringResource(R.string.button_save))
                     }
-                    Button(
+                    OutlinedButton(
                         onClick = { value = viewModel.getCurrentText() }
                     ) {
                         Text(text = stringResource(R.string.button_load_current_text))
                     }
-                    Button(
+                    OutlinedButton(
                         onClick = { value = "" }
                     ) {
                         Text(text = stringResource(R.string.button_clear))
